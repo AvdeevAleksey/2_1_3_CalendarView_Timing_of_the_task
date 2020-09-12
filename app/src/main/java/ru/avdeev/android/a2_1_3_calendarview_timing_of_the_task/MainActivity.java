22a7cc264e41de93e974147c0126893a94e8c85d
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-12 18:29:11 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a2_1_3_calendarview_timing_of_the_task/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.avdeev.android.a2_1_3_calendarview_timing_of_the_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
