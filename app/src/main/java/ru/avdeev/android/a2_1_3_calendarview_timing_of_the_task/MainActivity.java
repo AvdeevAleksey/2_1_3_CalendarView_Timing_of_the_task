2d2e8ac66220217bbc3cb80b6ec81ba2e436bdc8
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-12 21:47:18 +0300

    This is what the life-giving cross does.

--- app/src/main/java/ru/avdeev/android/a2_1_3_calendarview_timing_of_the_task/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a2_1_3_calendarview_timing_of_the_task/MainActivity.java
@@ -1,14 +1,21 @@
 package ru.avdeev.android.a2_1_3_calendarview_timing_of_the_task;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+//import android.icu.util.GregorianCalendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonOk;
     private CalendarView calendarChooseStartDate;
     private CalendarView calendarChooseEndDate;
+    private long startDate;
+    private String startDateTxt;
+    private long endDate;
+    private String endDateTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +44,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnOkClocked (View view) {
-
+        if (!(startDate==endDate)) {
+            if (startDate > endDate) {
+                Toast.makeText(MainActivity.this, getText(R.string.mistake), Toast.LENGTH_LONG).show();
+                buttonChooseStartDate.setText(R.string.chooseStartDate);
+                buttonChooseEndDate.setText(R.string.chooseEndDate);
+            } else {
+                Toast.makeText(MainActivity.this, getText(R.string.chooseStartDate) + startDateTxt + "\n" + getText(R.string.chooseEndDate) + endDateTxt, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this,getText(R.string.gross_error), Toast.LENGTH_LONG).show();
+        }
     }
 
     public void btnChooseStartDateClicked (View view) {
         calendarChooseStartDate.setVisibility(View.VISIBLE);
         calendarChooseEndDate.setVisibility(View.GONE);
+        buttonOk.setVisibility(View.GONE);
+
+        calendarChooseStartDate.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //@RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                startDateTxt = i2+"-"+ Integer.toString(i1+1) +"-"+i;
+                buttonChooseStartDate.setText(getText(R.string.chooseStartDate) + startDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                startDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+                buttonOk.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public void btnChooseEndDateClicked (View view) {
         calendarChooseEndDate.setVisibility(View.VISIBLE);
         calendarChooseStartDate.setVisibility(View.GONE);
+        buttonOk.setVisibility(View.GONE);
+
+        calendarChooseEndDate.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //@RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                endDateTxt = i2+"-"+ Integer.toString(i1+1) +"-"+i;
+                buttonChooseEndDate.setText(getText(R.string.chooseEndDate) + endDateTxt);
+                GregorianCalendar gregorianCalendar = new GregorianCalendar();
+                gregorianCalendar.set(i, i1, i2);
+                endDate = gregorianCalendar.getTimeInMillis();
+                calendarView.setVisibility(View.GONE);
+                buttonOk.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
